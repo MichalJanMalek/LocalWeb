3c262db7f7ffb08b3bc9f5adfc262c2bb1c6d01b
Michal Malek <malek.michal.j@gmail.com>
2023-01-26 15:03:32 -0600

    quick

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,30 @@
 			<p class = "text">This is my personal website to showcase my projects and to learn more about me. </p>
 		</div>	
 		<div>
-			
+<CCarousel controls indicators>
+  <CCarouselItem>
+    <img class="d-block w-100" src="/smartspace.png" alt="slide 1"/>
+    <CCarouselCaption class="d-none d-md-block">
+      <h5>First slide label</h5>
+      <p>Some representative placeholder content for the first slide.</p>
+    </CCarouselCaption>
+  </CCarouselItem>
+  <CCarouselItem>
+    <img class="d-block w-100" src="/images/react.jpg" alt="slide 2"/>
+    <CCarouselCaption class="d-none d-md-block">
+      <h5>First slide label</h5>
+      <p>Some representative placeholder content for the first slide.</p>
+    </CCarouselCaption>
+  </CCarouselItem>
+  <CCarouselItem>
+    <img class="d-block w-100" src="/images/angular.jpg" alt="slide 3"/>
+    <CCarouselCaption class="d-none d-md-block">
+      <h5>First slide label</h5>
+      <p>Some representative placeholder content for the first slide.</p>
+    </CCarouselCaption>
+  </CCarouselItem>
+</CCarousel>
+
 		</div>
     </main>
 </template>
